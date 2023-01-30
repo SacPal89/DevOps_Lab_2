2c8b50886531c636008b4be04c3ddb802adc0a05
SacPal89 <113502240+SacPal89@users.noreply.github.com>
2023-01-30 10:54:20 +0530

    Error Resolved

--- Palindrome.java
+++ Palindrome.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in)
+    Scanner sc = new Scanner(System.in);
     String str = sc.nextLine();
     String reverseStr = "";
     
